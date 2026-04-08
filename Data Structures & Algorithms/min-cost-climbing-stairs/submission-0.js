5946300dd22195426b88ecc62d8e0e3b1bb09649
Abhay Gupta <abhaysgpt@gmail.com>
2026-04-08 23:52:37 +0530

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} cost
+     * @return {number}
+     */
+    minCostClimbingStairs(cost) {
+        
+        function dfs(i){
+            if(i >= cost.length) return 0;
+
+            return cost[i] + Math.min(dfs(i + 1), dfs(i + 2))
+        }
+
+        return Math.min(dfs(0), dfs(1))
+    }
+}
